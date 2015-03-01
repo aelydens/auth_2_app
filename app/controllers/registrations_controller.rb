b6391b79489c6f9074e1fd9bc59757e0a029edf5
Annie Lydens <aelydens@gmail.com>
2015-03-01 16:16:34 -0700

    Users can sign up and log out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,15 @@
 class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:email, :password))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You've signed up!"
+    else
+      render :new
+    end
+  end
 end
